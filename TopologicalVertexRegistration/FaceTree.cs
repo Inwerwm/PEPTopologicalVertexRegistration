821b006d3c59d4f75d7ab8adc65b5c41b20703b7
Inwerwm <saclainvervm@gmail.com>
2020-06-20 13:01:09 +0900

    ツリー用クラス

--- TopologicalVertexRegistration/FaceTree.cs
+++ TopologicalVertexRegistration/FaceTree.cs
@@ -3,8 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TopologicalVertexRegistration
 {
@@ -12,26 +10,82 @@ namespace TopologicalVertexRegistration
     {
         public int Count { get; private set; }
         public FaceNode Root { get; private set; }
-        public ReadOnlyCollection<IPXFace> Faces { get;}
+        public ReadOnlyCollection<FaceNode> FaceNodes { get; }
+        private List<FaceNode> UnalignedNodes { get; }
 
-        public FaceTree(IEnumerable<IPXFace> faces,IPXFace root)
+        public FaceTree(IEnumerable<IPXFace> faces, IPXFace root)
         {
             if (!faces.Contains(root))
                 throw new ArgumentException("root面はfacesコンテナ内に存在していなければなりません。");
 
-            Faces = new ReadOnlyCollection<IPXFace>(faces.ToList());
+            UnalignedNodes = faces.Select(f => new FaceNode(f)).ToList();
+            FaceNodes = new ReadOnlyCollection<FaceNode>(UnalignedNodes);
+            Root = new FaceNode(root);
         }
 
-        private void SetNeighborRecurcive()
+        private void SetNeighborRecurcive(FaceNode node)
+        {
+
+
+        }
     }
 
-    class FaceNode
+    class FaceNode : IEquatable<FaceNode>
     {
         public IPXFace Face { get; set; }
-        public bool Flag { get; set; }
+        private List<PXEdge> edges;
+        internal List<PXEdge> Edges
+        {
+            get
+            {
+                if (edges == null)
+                    edges = PXEdge.FromFace(Face);
+                return edges;
+            }
+        }
+        public bool Flag { get; set; } = false;
         public bool IsBorder { get => Neighbor.Any(n => n == null); }
-        
+
         public FaceNode Parent { get; set; }
-        public FaceNode[] Neighbor { get; } = new FaceNode[3];
+        public List<FaceNode> Neighbor { get; } = new List<FaceNode>();
+
+        public FaceNode(IPXFace face)
+        {
+            Face = face;
+        }
+
+
+        public bool Equals(FaceNode other) => Face == other.Face;
+        public override bool Equals(object obj) => obj is FaceNode node && Equals(node);
+
+        public override int GetHashCode()
+        {
+            return Face.GetHashCode();
+        }
+    }
+
+    class PXEdge : IEquatable<PXEdge>
+    {
+        public IPXVertex Vertex1 { get; set; }
+        public IPXVertex Vertex2 { get; set; }
+        public List<IPXVertex> Vertices { get => new List<IPXVertex> { Vertex1, Vertex2 }; }
+        public (IPXVertex, IPXVertex) Pair { get => (Vertex1, Vertex2); }
+
+        public PXEdge(IPXVertex vertex1, IPXVertex vertex2)
+        {
+            Vertex1 = vertex1;
+            Vertex2 = vertex2;
+        }
+
+
+        public static List<PXEdge> FromFace(IPXFace f) => new List<PXEdge> { new PXEdge(f.Vertex1, f.Vertex2), new PXEdge(f.Vertex2, f.Vertex3), new PXEdge(f.Vertex3, f.Vertex1) };
+
+        public bool Equals(PXEdge other) => Vertex1 == other.Vertex1 && Vertex2 == other.Vertex2;
+        public override bool Equals(object obj) => obj is PXEdge edge && Equals(edge);
+
+        public override int GetHashCode()
+        {
+            return (Vertex1.GetHashCode() + Vertex2.GetHashCode()) / 2;
+        }
     }
 }
